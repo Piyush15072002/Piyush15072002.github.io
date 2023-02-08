310f16348b2186304e97ccad18ccf53652f44d46
PIYUSH PANT <piyushpant15@gmail.com>
2023-02-09 00:13:32 +0530

    Done

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -1,17 +1,44 @@
+import { useState } from 'react'
 import './index.scss'
+import AnimatedLetter from '../AnimatedLetters/index.js'
+
 
 
 const Home = () => {
+
+    const [letterClass, setLetterClass] = useState('text-animate')
+
+    const nameArray = ['P', 'i', 'y', 'u', 's', 'h', ' ', 'P', 'a', 'n', 't']
+
+    const jobArray = ['M', 'a', 'c', 'h', 'i', 'n', 'e', ' ', 'L', 'e', 'a', 'r', 'n', 'i', 'n', 'g', ' ', 'E', 'n', 'g', 'i', 'n', 'e', 'e', 'r', ',', ' ', 'A', 'K', 'A', ' ', 'D', 'a', 't', 'a', ' ', 'S', 'c', 'i', 'e', 'n', 't', 'i', 's', 't', ' ', 'b', 'u', 't', ' ', 'T', 'e', 'c', 'h', 'n', 'i', 'c', 'a', 'l']
+
+
+
+
+
     return (
         <div className="container home-page">
 
             <div className="text-zone">
-                <h1>Hi,<br />I'm Piyush Pant</h1>
+                <h1>
+                    <span className={letterClass}>N</span>
+                    <span className={`${letterClass} _12`}>a</span>
+                    <span className={`${letterClass} _13`}>m</span>
+                    <span className={`${letterClass} _14`}>a</span>
+                    <span className={`${letterClass} _15`}>s</span>
+                    <span className={`${letterClass} _16`}>t</span>
+                    <span className={`${letterClass} _17`}>e,</span>
+
+                    <br />
+
+                    <span className={`${letterClass} _18`}>I</span>
+                    <span className={`${letterClass} _19`}>'m </span> <AnimatedLetter letterClass={letterClass} strArray={nameArray} index={20} />
+                </h1>
 
                 <br />
 
-                <h2> <i>
-                    Machine Learning Engineer, AKA Data Scientist but Technical</i>
+                <h2>
+                    <AnimatedLetter letterClass={letterClass} strArray={jobArray} index={31} />
                 </h2>
             </div>
 
