225ef79c85e1c90784e29da19cb6124b12a9bb24
Piyush Pant <piyushpant15@gmail.com>
2023-02-10 17:26:51 +0530

    Done

--- src/components/Contact/index.js
+++ src/components/Contact/index.js
@@ -3,7 +3,17 @@ import Loader from 'react-loaders'
 import AnimatedLetters from '../AnimatedLetters/index.js'
 import { useEffect, useRef, useState } from 'react'
 import emailjs from '@emailjs/browser';
+import { MapContainer, Marker, Popup, TileLayer } from 'react-leaflet';
+import 'leaflet/dist/leaflet.css';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faLocation } from '@fortawesome/free-solid-svg-icons';
+import marker from '../../assets/images/marker.svg';
+import { Icon } from 'leaflet'
 
+const myIcon = new Icon({
+    iconUrl: marker,
+    iconSize: [32, 32]
+})
 
 
 const Contact = () => {
@@ -53,7 +63,7 @@ const Contact = () => {
                     </h1>
 
                     <p>
-                        Please feel free to reach out to me for any Job/Internship offers, project collaboration, tutoring, or any other help. I would be truly delighted by your effort.
+                        Please feel free to reach out to me for any Job offers, project collaboration, or any other help.
 
                     </p>
                     <p>
@@ -87,6 +97,38 @@ const Contact = () => {
                         </form>
 
                     </div>
+
+                </div>
+
+
+                <div className="info-map">
+                    Piyush Pant,
+                    <br />
+                    <FontAwesomeIcon icon={faLocation} alt="location" /> India
+                    <br />
+                    <span>
+                        piyushpant15@gmail.com
+                    </span>
+                </div>
+
+
+                <div className="map-wrap">
+
+                    <MapContainer center={[19.0760, 72.8777]} zoom={4}>
+
+                        <TileLayer url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png" />
+
+                        <Marker icon={myIcon} position={[19.0760, 72.8777]}>
+
+                            <Popup> <b style={{ fontSize: "12px" }} >
+                                Let's meet for a coffee! </b>
+                            </Popup>
+
+                        </Marker>
+
+                    </MapContainer>
+
+
 
                 </div>
 
